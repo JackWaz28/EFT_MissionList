9423dfa8d6c7d8a745e62b151d94d157faae983d
speedturtle28 <jackw4@vt.edu>
2022-01-06 19:43:02 +0000

    working on the html and css for the homepage

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 /**
  * COMPONENT
  */
 export const Home = props => {
-  const {username} = props
+  const { username } = props
 
   return (
     <div>
-      <h3>Welcome, {username}</h3>
+      <div>
+        <h3>Welcome, {username}</h3>
+      </div>
+      <div class="traders-grid-homepage">
+        <img class="traders-grid-items" src="Prapor.jpg"></img>  
+        <img class="traders-grid-items" src="Therapist.jpg"></img>  
+        <img class="traders-grid-items" src="Fence.jpg"></img>  
+        <img class="traders-grid-items" src="Skier.jpg"></img>  
+        <img class="traders-grid-items" src="Peacekeeper.jpg"></img>  
+        <img class="traders-grid-items" src="Mechanic.jpg"></img>  
+        <img class="traders-grid-items" src="Ragman.jpg"></img>  
+        <img class="traders-grid-items" src="Jaeger.jpg"></img>  
+      </div>
     </div>
   )
 }
